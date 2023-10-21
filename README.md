f803dab7346ba0787c34e79ebf8c0605b6431f39
PeanutHome <techpeanuthome@gmail.com>
2023-10-21 16:57:54 +0630

    Inital Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# viewchart
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/viewchart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.viewchart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
